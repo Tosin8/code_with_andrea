20f4b9f9d8f0e83d8565a9fc020d35d915f022da
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-18 21:28:21 +0100

    Updating Implicit Animation sample

--- lib/flutter_animation/animation.dart
+++ lib/flutter_animation/animation.dart
@@ -1,25 +1,37 @@
 import 'package:flutter/material.dart';
 
+// implicit animation
 class animate8 extends StatefulWidget {
   @override
   State<animate8> createState() => _animate8State();
 }
 
 class _animate8State extends State<animate8> {
-  final double _width = 200;
-  final double _height = 200;
-  final Color _color = Colors.red;
+  final double _width; 
+  final double _height; 
+  final Color _color; 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: AnimatedContainer(
           width: _width,
-          height: _height,
+          height: _height, 
           color: _color,
           duration: const Duration(milliseconds: 500),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.play_arrow), 
+        onTap:(){
+          setState((){
+            _width ? 200:300, 
+            _height ? 100:150, 
+            _color ? Colors.green: Colors.yellow, 
+          }
+          ), 
+        }, 
+      ), 
     );
   }
 }
