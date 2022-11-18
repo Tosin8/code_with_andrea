9fdca913056b98301632420cee83b6f9703f56bd
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-18 22:26:26 +0100

    Update animation.dart

--- lib/flutter_animation/animation.dart
+++ lib/flutter_animation/animation.dart
@@ -7,31 +7,23 @@ class animate8 extends StatefulWidget {
 }
 
 class _animate8State extends State<animate8> {
-  final double _width; 
-  final double _height; 
-  final Color _color; 
+  late final double _width, _height;
+  late final Color _color;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: AnimatedContainer(
-          width: _width,
-          height: _height, 
-          color: _color,
+          width: 200,
+          height: 200,
+          color: Colors.red,
           duration: const Duration(milliseconds: 500),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.play_arrow), 
-        onTap:(){
-          setState((){
-            _width ? 200:300, 
-            _height ? 100:150, 
-            _color ? Colors.green: Colors.yellow, 
-          }
-          ), 
-        }, 
-      ), 
+        child: const Icon(Icons.play_arrow),
+        onPressed: () {},
+      ),
     );
   }
 }
